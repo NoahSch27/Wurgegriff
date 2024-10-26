b492a4f507a8c2cbcb2783a154879c53ac3d5dc2
Snow <noah.sch2007@gmail.com>
2024-10-26 15:44:47 +0200

    Test

--- src/main/java/Wurgegriff/MinecraftMod.java
+++ src/main/java/Wurgegriff/MinecraftMod.java
@@ -1,10 +1,15 @@
 package Wurgegriff;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MinecraftMod implements ModInitializer {
+    public static final String MOD_ID = "wurgegriff";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
+        LOGGER.info("Mod initzializeing");
     }
 }
